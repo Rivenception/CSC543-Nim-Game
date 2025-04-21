208474289b2225252327493b399a18d2f554158e
emmanuelalesna <emmanuelalesna@gmail.com>
2025-04-21 15:22:34 -0400

    handling of signup

--- public_html/assets/js/ajaxSignup.js
+++ public_html/assets/js/ajaxSignup.js
@@ -6,6 +6,15 @@ const signup = (e) => {
   ajax.send(
     JSON.stringify({ username: e.form[0].value, password: e.form[1].value })
   );
+  ajax.onreadystatechange = () => {
+    if (ajax.readyState === XMLHttpRequest.DONE) {
+      if (ajax.status === 200) {
+        console.log(`Signup success: ${ajax.response}`);
+      } else {
+        console.log(`Error ${ajax.status}: ${ajax.response}`);
+      }
+    }
+  };
 };
 
 export { signup };
