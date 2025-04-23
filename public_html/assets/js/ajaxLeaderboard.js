ed85b315970ac309438f8dcfa9a881d8e1d1e168
Corwin Van Deusen <corwinvandeusen1205@gmail.com>
2025-04-23 13:43:38 +0000

    implemented leaderboard

---
+++ public_html/assets/js/ajaxLeaderboard.js
@@ -0,0 +1,21 @@
+let ajax = new XMLHttpRequest();
+
+const getStatistics = async (e) => {
+  return new Promise((resolve, reject) => {
+    ajax.open('POST', '/leaderboard');
+    ajax.setRequestHeader('Content-Type', 'text/plain');
+    ajax.onload = () => {
+      if (ajax.status == 200) {
+        console.log(`Got Statistics: ${ajax.response}`);
+        resolve(ajax.response);
+      } else {
+        console.log(`Error ${ajax.status}: ${ajax.response}`);
+        reject(ajax.statusText);
+      }
+    };
+    ajax.onerror = () => reject(ajax.statusText);
+    ajax.send()
+  });
+};
+
+export { getStatistics };
